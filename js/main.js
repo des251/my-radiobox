20f1cc93eff478bed2ade329769db295c8b61ebc
Denis <shcherbinskyd@gmail.com>
2020-11-21 14:48:24 +0300

    Made navbar sticky , and change color on scroll

--- js/main.js
+++ js/main.js
@@ -3,3 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.sidenav');
   var instances = M.Sidenav.init(elems);
 });
+
+// Sticky navbar
+function makeNavSticky() {
+  var flag = true;
+  var el = document.getElementById('main-nav');
+  window.addEventListener('scroll', function() {
+    if (this.pageYOffset > 0) {
+      if (flag) {
+        el.classList.remove('transparent');
+        el.classList.add('cyan', 'accent-4');
+        flag = false;
+      }
+    };
+    if (this.pageYOffset < 100) {
+      if (!flag) {
+        el.classList.remove('cyan', 'accent-4')
+        el.classList.add('transparent');
+        flag = true;
+      }
+    };
+  });
+};
+
+makeNavSticky();
